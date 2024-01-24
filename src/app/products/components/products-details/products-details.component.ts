cdf57917b5564b42836a4ee08981af51dd021ffb
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-01-24 16:30:45 +0100

    Project Structure

---
+++ src/app/products/components/products-details/products-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-details',
+  templateUrl: './products-details.component.html',
+  styleUrl: './products-details.component.css'
+})
+export class ProductsDetailsComponent {
+
+}
